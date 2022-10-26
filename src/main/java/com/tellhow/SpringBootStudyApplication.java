c80a9008cdfc74478b96aa34c8eb5797c90473e3
wujianghao <755407053@qq.com>
2022-10-26 22:23:22 +0800

    给项目添加日志功能

--- src/main/java/com/tellhow/SpringBootStudyApplication.java
+++ src/main/java/com/tellhow/SpringBootStudyApplication.java
@@ -1,13 +1,15 @@
 package com.tellhow;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootStudyApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStudyApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootStudyApplication.class, args);
     }
 
 }
