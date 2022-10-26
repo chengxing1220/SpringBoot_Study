12d5ff3e70be18e97019d67f1fa411db90143080
wujianghao <P2ssw0rd>
2022-10-26 18:19:03 +0800

    Initial commit

---
+++ src/test/java/com/tellhow/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tellhow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
