e31afddd640a82c589c663174150ea95d2d6e56c
wujianghao <755407053@qq.com>
2022-11-16 20:52:29 +0800

    给属性赋值(基本数据类型和String)

--- src/main/java/com/tellhow/review/spring_annotation/attribute/pojo/Disaster.java
+++ src/main/java/com/tellhow/review/spring_annotation/attribute/pojo/Disaster.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.PropertySource;
  *      1、value=字符串，直接将字符串赋值给属性
  *      2、value="#{}",大括号里可以写表达式
  *      3、value="${}",大括号里可以取环境变量中的值
- *      要想向将配置文件中的值赋给属性，我们要先将配置文件的值加到环境变量中。这里需要用到@PropertySource注解，它的value属性指定配置文件的类路径例如@PropertySource(value ={"classpath:/myconfig.properties"})，@PropertySource注解一般加在配置类类名上
+ *      要想向将配置文件中的值赋给属性，我们要先将配置文件的值加到环境变量中。这里需要用到@PropertySource注解，它的value属性指定配置文件的类路径例如:@PropertySource(value ={"classpath:/myconfig.properties"})，@PropertySource注解一般加在配置类类名上
  */
 public class Disaster {
     @Value("祸殃")
